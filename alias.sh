facc8a76a2681fe37e171e7d612184eca84779ba
James <jlove7714@gmail.com>
2020-10-27 12:29:04 -0400

    Added main compose up

--- alias.sh
+++ alias.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env bash
 
+sudo docker-compose up -d
 path=$(pwd)/docker_compose.yml
 echo "Alias for parrot container"
 echo alias parrot="docker-compose -f $path run parrot" >> $HOME/.bash_aliases
